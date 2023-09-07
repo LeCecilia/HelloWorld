28d25f92831896af5cd6c3c0c5beeef926474485
Cecilia Le <cecilia.le@mrjaffesclass.com>
2023-09-07 11:08:44 -0700

    Initial commit

---
+++ DataType.java
@@ -0,0 +1,10 @@
+public class DataType 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("What's Up") ;
+        int i; 
+        double f = 2.5;
+        boolean flag = true;
+    }
+}
