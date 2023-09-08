42a4e3560c2eb7b67c4295a743e115c25d393fb3
Cecilia Le <cecilia.le@mrjaffesclass.com>
2023-09-08 11:23:03 -0700

    Commit message

--- DataType.java
+++ DataType.java
@@ -6,5 +6,11 @@ public class DataType
         int i; 
         double f = 2.5;
         boolean flag = true;
+        i = 10;
+        f += i;
+        flag = !flag;
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(flag);
     }
 }
